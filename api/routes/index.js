93d9936319f77373a826be2d67e6e4d4ea52876a
Pavel Virskiy <me@pashinblog.ru>
2021-11-11 19:52:12 +0300

    Added /work/items.

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,5 +5,6 @@ api.use(require('./auth-router'));
 api.use(require('./debug-router'));
 api.use(require('./healtchcheck-router'));
 api.use(require('./inventory-router'));
+api.use(require('./work-router'));
 
 module.exports = api;
