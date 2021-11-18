7c3261040336a7cb364ca43f093aee32dedf3aea
Igor Sergeev <igor@tomatstudio.ru>
2021-11-18 16:35:02 +0300

    Обработка отсчёта времени при обновлении страницы

--- frontend/src/components/ButtonsBlock.vue
+++ frontend/src/components/ButtonsBlock.vue
@@ -1,13 +1,14 @@
 <template>
   <div v-if="!isLoad">
-    <user-status :status="workStatus" :timerCount="timerCount" />
+   
+    <div v-if="workStatus">
+      <p>Вы собираете травы. {{ timerText }}</p>    
+    </div>
+
     <b-button-group>
       <b-button v-if="!workStatus" @click.prevent="startWork" variant="info"
         >Собирать травы</b-button
-      >
-      <b-button @click.prevent="getStatus" variant="warning"
-        >Проверить токен</b-button
-      >
+      >      
     </b-button-group>
   </div>
   <b-spinner v-else variant="primary" type="grow" label="Spinning"></b-spinner>
@@ -16,60 +17,52 @@
 <script>
 import {mapGetters} from 'vuex';
 import User from '@/api/user';
-import UserStatus from '@/components/UserStatus.vue';
 export default {
   name: 'ButtonsBlock',
-  components: {
-    UserStatus,
-  },
+
   data() {
     return {
-      timerCount: 0,
       isLoad: true,
     };
   },
   computed: {
     ...mapGetters(['workStatus']),
+    timerText() {      
+      const min = Math.floor((this.timeDiffernce / 1000 / 60) << 0);
+      const sec = Math.floor((this.timeDiffernce / 1000) % 60);
+      return `осталось работать: ${min} мин ${sec} сек`;
+    },
+    timeDiffernce() {
+      return this.workStatus?.timeEnd - new Date().getTime();
+    },
   },
-  created() {
-    this.getStatus();
+  async created() {
+    await this.getStatus();
+    if (this.workStatus) {
+      this.stopWork();
+    }
   },
   methods: {
     async getStatus() {
-      const respone = await this.$store.dispatch(
-        'getStatus',
-        localStorage.getItem('accessToken'),
-      );
+      const respone = await this.$store.dispatch('getStatus', localStorage.getItem('accessToken'));
       this.isLoad = false;
       return respone.data;
     },
 
-    async check() {
-      try {
-        const response = await User.checkToken(
-          localStorage.getItem('accessToken'),
-        );
-        console.log(response);
-      } catch (e) {
-        console.log(e);
-      }
+    async startWork() {
+      const response = await User.startWork(localStorage.getItem('accessToken'));      
+      this.$store.commit('setStatus', response.data.cycle);
+      this.stopWork();
     },
 
-    async startWork() {
-      const response = await User.startWork(
-        localStorage.getItem('accessToken'),
-      );
-      this.$store.commit('setStatus', response.data.cycle);
-      const timeDifference = this.workStatus.timeEnd - Date.now() + 1000;
-      this.timerCount = timeDifference;
-
+    stopWork() {
       setTimeout(() => {
         this.isLoad = true;
         this.getStatus().then(resp => {
           this.$store.commit('setWorkResult', resp.previous);
-          this.$store.dispatch('getTable', localStorage.getItem('accessToken'));
+          this.$store.dispatch('getTable', localStorage.getItem('accessToken'));          
         });
-      }, timeDifference);
+      }, this.timeDiffernce);
     },
   },
 };
