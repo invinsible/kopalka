bc3224a7f8d5a76c1eeb8c85ede1a8837eac7425
Igor Sergeev <igor@tomatstudio.ru>
2021-11-18 17:15:37 +0300

    Добавил setInterval

--- frontend/src/components/ButtonsBlock.vue
+++ frontend/src/components/ButtonsBlock.vue
@@ -2,7 +2,7 @@
   <div v-if="!isLoad">
     <p v-if="workStatus">Вы собираете травы. {{ timerText }}</p>
     <p v-if="workResult && !workStatus">{{ resultText }}</p>    
-    <b-button v-if="!workStatus" @click.prevent="startWork" variant="info">Собирать травы</b-button>    
+    <b-button v-if="!workStatus" @click.prevent="startWork" variant="info">Собирать травы</b-button>
   </div>
   <b-spinner v-else variant="primary" type="grow" label="Spinning"></b-spinner>
 </template>
@@ -15,14 +15,16 @@ export default {
 
   data() {
     return {
-      isLoad: true,     
+      isLoad: true,
+      interval: 0,
     };
   },
   computed: {
     ...mapGetters(['workStatus', 'workResult']),
     timerText() {      
       const min = Math.floor((this.timeDiffernce / 1000 / 60) << 0);
-      const sec = Math.floor((this.timeDiffernce / 1000) % 60);
+      const sec = Math.floor((this.timeDiffernce / 1000) % 60); 
+      // this.timeInterval(min, sec);
       return `осталось работать: ${min} мин ${sec} сек`;
     },
     timeDiffernce() {
@@ -40,13 +42,19 @@ export default {
     }
   },
   methods: {
+    timeInterval(min, sec) {
+      this.interval = setInterval(() => {
+        sec -= 1;
+        console.log(`осталось работать: ${min} мин ${sec} сек`);
+      }, 1000);
+    },
     async getStatus() {
       const respone = await this.$store.dispatch('getStatus', localStorage.getItem('accessToken'));
       this.isLoad = false;
       return respone.data;
     },
 
-    async startWork() {
+    async startWork() {     
       const response = await User.startWork(localStorage.getItem('accessToken'));      
       this.$store.commit('setStatus', response.data.cycle);
       this.stopWork();
@@ -55,6 +63,7 @@ export default {
     stopWork() {
       setTimeout(() => {
         this.isLoad = true;
+        clearInterval(this.interval);
         this.getStatus().then(resp => {
           this.$store.commit('setWorkResult', resp.previous);
           this.$store.dispatch('getTable', localStorage.getItem('accessToken'));          
